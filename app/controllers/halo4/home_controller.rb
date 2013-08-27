a89092ecf3e05903d379ab9d919bab3fb55ea346
Alex Reed <alexreed@outlook.com>
2013-08-27 00:13:40 -0700

    Halo 4 stuff

---
+++ app/controllers/halo4/home_controller.rb
@@ -0,0 +1,7 @@
+class Halo4::HomeController < ApplicationController
+
+	def index
+		
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 BranchApp::Application.routes.draw do
   root :to => 'home#welcome'
+
+  get '/halo4/' => 'halo4/home#index', as: :halo4_home
 end
