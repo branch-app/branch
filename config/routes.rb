0303ddca44981c5057a833a1422fd039a31f5c4e
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-02 23:33:53 -0700

    Roots and application layout tweak

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 BranchApp::Application.routes.draw do
 
   # Core
-  root :to => 'home#welcome'
+  root :to => 'home#index'
+  get '/welcome' => 'home#welcome', as: :home_welcome
+
+  # Account
+  get '/user/signin' => 'user#signin', as: :user_signin
+  get '/user/register' => 'user#register', as: :user_register
+  get '/user/:username' => 'user#view', as: :user_view
+
+  # Search
   get '/search/:focus/' => 'search#search', as: :search_query, defaults: { focus: 'all' }
 
   # Halo 4
