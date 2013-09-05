8c37ec0d1daf4ac1f363f6e987c43c9de683a064
Alex Reed <alexreed@outlook.com>
2013-09-04 23:37:55 -0700

    Wrote a time helper

--- app/helpers/halo4/game_history_helper.rb
+++ app/helpers/halo4/game_history_helper.rb
@@ -9,4 +9,28 @@ module Halo4::GameHistoryHelper
 		end
 	end
 
+	def duration_to_friendly(duration)
+		str = ''
+		
+		# Hours
+		unless duration.hour == 0
+			str += duration.hour.to_s + ' Hour, ' if duration.hour == 1
+			str += duration.hour.to_s + ' Hours, ' if duration.hour > 1 || duration.hour < 1
+		end
+
+		# Minutes
+		unless duration.min == 0
+			str += duration.min.to_s + ' Minute, ' if duration.min == 1
+			str += duration.min.to_s + ' Minutes, ' if duration.min > 1 || duration.min < 1
+		end
+
+		# Seconds
+		unless duration.sec == 0
+			str += 'and ' + duration.sec.to_s + ' Second' if duration.sec == 1
+			str += 'and ' + duration.sec.to_s + ' Seconds' if duration.sec > 1 || duration.sec < 1
+		end
+
+		return str
+	end
+
 end
