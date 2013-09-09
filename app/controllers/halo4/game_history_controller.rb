6fd35fa8d2b59a0dea34523125ce60d9686d7aec
Alex Reed <alexreed@outlook.com>
2013-09-08 23:59:11 -0700

    Added commendation stuff

--- app/controllers/halo4/game_history_controller.rb
+++ app/controllers/halo4/game_history_controller.rb
@@ -15,35 +15,5 @@ class Halo4::GameHistoryController < Halo4::HomeController
 		@css_class = 'matchmaking'
 		@duration = Time.new(1994, 8, 18, 2, 45, 56, '+00:00')
 	end
-
-	private
-
-	def sub_view_to_friendly(sub_view)
-		return sub_view.gsub('-', ' ').titlecase
-	end
-
-	def sub_view_to_css_class(sub_view)
-		return sub_view.gsub('-', '_')
-	end
-
-	def validate_param(param, valid_responses = nil, default_response = nil)
-		return default_response || nil, false if param == nil || param.strip == ''
-		return param, true if valid_responses == nil || default_response == nil
-
-		# check valid responses
-		valid_responses.each { |r| return param, true if param.strip.downcase == r }
-		return default_response, false
-	end
-
-	def validate_numerical_param(param, min = 0, max = 0xFFFFFFFF)
-		param = param.to_i
-		return 0, false if param == nil
-
-		if param >= min && param <= max
-			return param, true
-		else
-			return 0, false
-		end
-	end
-
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,18 +15,15 @@ BranchApp::Application.routes.draw do
   # Halo 4
   get '/halo4/' => 'halo4/home#index', as: :halo4_home
   get '/halo4/servicerecord/:gamertag/' => 'halo4/service_record#index', as: :halo4_servicerecord
-
   get '/halo4/servicerecord/:gamertag/match-history' => 'halo4/game_history#index'
   get '/halo4/servicerecord/:gamertag/game-history(/:sub_view/:page)' => 'halo4/game_history#index', as: :halo4_gamehistory
-
   get '/halo4/servicerecord/:gamertag/match/:game_id' => 'halo4/game_history#view'
   get '/halo4/servicerecord/:gamertag/game/:game_id' => 'halo4/game_history#view', as: :halo4_viewgame
-
   get '/halo4/servicerecord/:gamertag/competitive-skill-rank' => 'halo4/competitive_skill_rank#index'
   get '/halo4/servicerecord/:gamertag/csr' => 'halo4/competitive_skill_rank#index', as: :halo4_csr
-  get '/halo4/servicerecord/:gamertag/competitive-skill-rank/:id' => 'halo4/competitive_skill_rank#details'
   get '/halo4/servicerecord/:gamertag/csr/:id' => 'halo4/competitive_skill_rank#details', as: :halo4_csr_details
-
+  get '/halo4/servicerecord/:gamertag/competitive-skill-rank/:id' => 'halo4/competitive_skill_rank#details'
+  get '/halo4/servicerecord/:gamertag/commendations(/:sub_view)' => 'halo4/commendations#index', as: :halo4_commendations
   get '/halo4/servicerecord/:gamertag/specializations' => 'halo4/specialization#index', as: :halo4_specialization
 
 end
