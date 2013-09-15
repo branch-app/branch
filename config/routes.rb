c724631b607316dfefe0549602549e164265c959
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-15 15:49:44 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 BranchApp::Application.routes.draw do
 
+  get "session/new"
+
+  get "session/create"
+
+  get "session/destroy"
+
   # Core
   root :to => 'home#index'
   get '/welcome' => 'home#welcome', as: :home_welcome
 
   # Account
-  get '/user/signin' => 'user#signin', as: :user_signin
-  get '/user/register' => 'user#register', as: :user_register
-  get '/user/:username' => 'user#view', as: :user_view
+  get '/user/signin' => 'user/session#new', as: :user_signin
+  get '/user/register' => 'user/user#new', as: :user_register
+  get '/user/:username' => 'user/home#index', as: :user_view
 
   # Search
   get '/search/:focus/' => 'search#search', as: :search_query, defaults: { focus: 'all' }
