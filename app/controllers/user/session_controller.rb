a945f42ccd1aaa6601ff7b1c39f443390e88f44a
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-15 15:49:28 -0700

    Added session and user controllers

---
+++ app/controllers/user/session_controller.rb
@@ -0,0 +1,10 @@
+class User::SessionController < User::HomeController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/user/user_controller.rb
@@ -0,0 +1,7 @@
+class User::UserController < User::HomeController
+  def new
+  end
+
+  def create
+  end
+end
