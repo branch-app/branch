2aef5ccf03d459e489ff1279f9645b49badc7977
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-16 22:42:41 -0700

    Added before filter to get username

--- app/controllers/user/account/home_controller.rb
+++ app/controllers/user/account/home_controller.rb
@@ -1,4 +1,10 @@
 class User::Account::HomeController < User::HomeController
+	before_filter :validate_username
+
+	def validate_username
+		@username = params[:username]
+	end
+
 	def index
 	end
 end
