333c4054029da7561e0265cc8ead6d788f934138
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-16 23:52:06 -0700

    Did some stuff on user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :gamertag_id, :name, :password, :role_id, :username
+	attr_accessible :email, :gamertag_id, :gamertag, :name, :password, :password_confirmation, :role_id, :username
+	attr_accessor :password_confirmation, :gamertag
+
+	validate_presence_of :email, :gamertag, :name
+
+	validates_length_of :username, in: 1..30
+	validates_length_of :name, in: 3..30
+
+	validates_uniqueness_of :email
+	validates_uniqueness_of :username
+
+	
 end
