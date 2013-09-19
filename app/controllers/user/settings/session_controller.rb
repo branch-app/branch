eb6f947ee7630216a71240b1ff2228f1fd6e2327
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-19 06:31:26 +0100

    sql query for loading user sessions

--- app/controllers/user/settings/session_controller.rb
+++ app/controllers/user/settings/session_controller.rb
@@ -1,5 +1,6 @@
 class User::Settings::SessionController < User::Settings::HomeController
 	def index
+		@sessions = Session.where('expired = FALSE AND user_id = ? AND expires_at > CURRENT_DATE', current_user.id)
 	end
 
 	def destroy
