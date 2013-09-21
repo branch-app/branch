ddd981965c73c6752d2c99eb1534ef8dae4bc361
Alex Reed (Xerax) <alexreed@outlook.com>
2013-09-22 00:21:50 +0100

    Added roles to the db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+roles = Role.create([
+	{
+		colour: '5cb85c',
+		description: 'Developer',
+		identifier: 3,
+		name: 'A Developer on The Tree.'
+	},
+	{
+		colour: '5bc0de',
+		description: 'User',
+		identifier: 2,
+		name: 'A User of Branch App.'
+	},
+	{
+		colour: '999999',
+		description: 'Validating',
+		identifier: 1,
+		name: 'A Validating User of Branch App.'
+	},
+	{
+		colour: 'd9534f',
+		description: 'Banned',
+		identifier: 0,
+		name: 'A Banned User of Branch App.'
+	}
+])
