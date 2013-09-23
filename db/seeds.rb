078bbe1d48390d6c0c0f0e5b4e020ce00c26b329
Alex Reed <alexreed@outlook.com>
2013-09-23 05:11:44 +0100

    updated database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-roles = Role.create([
-	{
-		colour: '5cb85c',
-		description: 'Developer',
-		identifier: 3,
-		name: 'A Developer on The Tree.'
-	},
-	{
-		colour: '5bc0de',
-		description: 'User',
-		identifier: 2,
-		name: 'A User of Branch App.'
-	},
-	{
-		colour: '999999',
-		description: 'Validating',
-		identifier: 1,
-		name: 'A Validating User of Branch App.'
-	},
-	{
-		colour: 'd9534f',
-		description: 'Banned',
-		identifier: 0,
-		name: 'A Banned User of Branch App.'
-	}
-])
+roles = Role.all
+if Role.count == 0
+	roles = Role.create([
+		{
+			colour: '5cb85c',
+			name: 'Developer',
+			identifier: 3,
+			description: 'A Developer on The Tree.'
+		},
+		{
+			colour: '5bc0de',
+			name: 'User',
+			identifier: 2,
+			description: 'A User of Branch App.'
+		},
+		{
+			colour: '999999',
+			name: 'Validating',
+			identifier: 1,
+			description: 'A Validating User of Branch App.'
+		},
+		{
+			colour: 'd9534f',
+			name: 'Banned',
+			identifier: 0,
+			description: 'A Banned User of Branch App.'
+		}
+	])
+end
