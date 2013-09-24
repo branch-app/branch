9f2ca26f96669dd68d9e6df55e6ff9c22a078168
Alex Reed <alexreed@outlook.com>
2013-09-24 03:12:55 +0100

    blog post relationships

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,6 @@
 class BlogPost < ActiveRecord::Base
   attr_accessible :blog_category_id, :slug, :full_body, :is_published, :name, :short_body, :user_id
+
+  belongs_to :user
+  belongs_to :blog_category
 end
