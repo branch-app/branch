b290d2216d2bae2e82ac26cb8b4c06d4c215447e
Alex Reed <alexreed@outlook.com>
2013-09-24 04:12:34 +0100

    blog post view styling, and controller logic

--- app/controllers/blog/home_controller.rb
+++ app/controllers/blog/home_controller.rb
@@ -1,15 +1,32 @@
 class Blog::HomeController < ApplicationController
+
+	PER_PAGE = 3
+
 	def index
 		@page = params[:page].to_i
+		@focus = BlogCategory.find_by_slug(params[:focus])
+
 		if @page == nil || @page < 1
 			redirect_to(blog_home_path(page: 1))
 			return
 		end
+		if @focus == nil && params[:focus] != nil
+			redirect_to(blog_home_path(page: 1))
+			return
+		end
 
-		@blog_posts = BlogPost.where('is_published = TRUE').order('created_at DESC').limit(6)
+		@blog_posts = [ ]
+		if @focus
+			@blog_posts = BlogPost.where('is_published = TRUE AND blog_category_id = ?', @focus.id).order('created_at DESC').limit(PER_PAGE).offset((@page - 1) * (PER_PAGE - 1)) 
+		else
+			@blog_posts = BlogPost.where('is_published = TRUE').order('created_at DESC').limit(PER_PAGE).offset((@page - 1) * (PER_PAGE - 1)) 
+		end
 
 		@has_less = (@page > 1)
-		@has_more = (@blog_posts.length == 6)
+		if (@blog_posts.length == PER_PAGE)
+			@has_more = true
+			@blog_posts.pop
+		end
 
 		@previous_page = @page - 1
 		@previous_page = @page if !@has_less
