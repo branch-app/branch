9dda62231e03d580e659481b0a9024a37fd01d34
Alex Reed <alexreed@outlook.com>
2013-09-24 04:26:01 +0100

    blog view controller logic

--- app/controllers/blog/view_controller.rb
+++ app/controllers/blog/view_controller.rb
@@ -1,5 +1,18 @@
 class Blog::ViewController < Blog::HomeController
 	def index
-		
+		valid_blog_posts = BlogPost.where('is_published = TRUE AND slug = ?', params[:slug])
+		@blog_post = nil
+		valid_blog_posts.each do |blog_post|
+			if blog_post.created_at.year == params[:year].to_i && blog_post.created_at.month == params[:month].to_i && blog_post.created_at.day == params[:day].to_i
+				@blog_post = blog_post
+				break
+			end
+		end
+
+		if @blog_post == nil
+			redirect_to(blog_home_path)
+			return
+		end
+		@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
 	end
 end
