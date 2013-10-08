f7426ec2c1353077e6549e702aef5c9d90e60394
Alex Reed (Xerax) <alexreed@outlook.com>
2013-10-08 13:16:32 +0100

    more cleanup

--- app/helpers/halo4/game_history_helper.rb
+++ app/helpers/halo4/game_history_helper.rb
@@ -1,9 +1,9 @@
 module Halo4::GameHistoryHelper
 	def update_pagination(current_page, direction)
-		if direction == 'older'
-			return current_page if current_page == 0
+		if (direction == 'older')
+			return current_page if (current_page == 0)
 			return current_page - 1
-		elsif direction == 'newer'
+		elsif (direction == 'newer')
 			return current_page + 1
 		end
 	end
@@ -12,21 +12,21 @@ module Halo4::GameHistoryHelper
 		str = ''
 		
 		# Hours
-		unless duration.hour == 0
-			str += duration.hour.to_s + ' Hour, ' if duration.hour == 1
-			str += duration.hour.to_s + ' Hours, ' if duration.hour > 1 || duration.hour < 1
+		unless (duration.hour == 0)
+			str += duration.hour.to_s + ' Hour, ' if (duration.hour == 1)
+			str += duration.hour.to_s + ' Hours, ' if (duration.hour > 1 || duration.hour < 1)
 		end
 
 		# Minutes
-		unless duration.min == 0
-			str += duration.min.to_s + ' Minute, ' if duration.min == 1
-			str += duration.min.to_s + ' Minutes, ' if duration.min > 1 || duration.min < 1
+		unless (duration.min == 0)
+			str += duration.min.to_s + ' Minute, ' if (duration.min == 1)
+			str += duration.min.to_s + ' Minutes, ' if (duration.min > 1 || duration.min < 1)
 		end
 
 		# Seconds
-		unless duration.sec == 0
-			str += 'and ' + duration.sec.to_s + ' Second' if duration.sec == 1
-			str += 'and ' + duration.sec.to_s + ' Seconds' if duration.sec > 1 || duration.sec < 1
+		unless (duration.sec == 0)
+			str += 'and ' + duration.sec.to_s + ' Second' if (duration.sec == 1)
+			str += 'and ' + duration.sec.to_s + ' Seconds' if (duration.sec > 1 || duration.sec < 1)
 		end
 
 		return str
--- app/helpers/user/user_helper.rb
+++ app/helpers/user/user_helper.rb
@@ -1,5 +1,5 @@
 module User::UserHelper
 	def has_validation_errors(errors, type)
-		return 'display: none;' if errors.messages[type].length == 0
+		return 'display: none;' if (errors.messages[type].length == 0)
 	end
 end
