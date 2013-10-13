8c9ff8000446602c379ad2d4dc76269cf737a212
Alex Reed (Xerax) <alexreed@outlook.com>
2013-10-13 16:15:41 +0100

    stats loading in the halo4 home controller

--- app/controllers/halo4/home_controller.rb
+++ app/controllers/halo4/home_controller.rb
@@ -4,6 +4,15 @@ class Halo4::HomeController < ApplicationController
 	@gamertag = nil
 	def get_gamertag
 		@gamertag = params[:gamertag]
+
+		# pull stats
+		@service_record = H4Api.get_player_service_record(@gamertag)
+
+		if (@service_record == nil || @service_record[:continue] == false || @service_record["StatusCode"] != 1)
+			flash[:failure] = "We couldn't load stats for the gamertag #{@gamertag}, sorry :("
+			redirect_to(root_url())
+			return
+		end
 	end
 
 	def index
