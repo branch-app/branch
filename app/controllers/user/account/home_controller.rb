1300531873901d272afc5a8c98083948175a3be8
Alex Reed (Xerax) <alexreed@outlook.com>
2013-10-23 20:46:46 +0100

    account home controller gets halo 4 metadata

--- app/controllers/user/account/home_controller.rb
+++ app/controllers/user/account/home_controller.rb
@@ -1,8 +1,13 @@
 class User::Account::HomeController < User::HomeController
 	include Halo4::HomeHelper
 	before_filter :get_account
+	before_filter :get_h4_metadata
 	def get_account
 		@account = User.find_by_username(params[:id])
+	end
+
+	def get_h4_metadata
+		@metadata = H4Api.get_meta_data()
 	end
 
 	def index
