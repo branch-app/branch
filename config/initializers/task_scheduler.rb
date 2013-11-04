53a05bf7705ab28a725664a7ac4bd4f1c31aa244
Alex Reed (Xerax) <alexreed@outlook.com>
2013-11-04 00:43:45 +0000

    update those services

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,9 +2,14 @@ scheduler = Rufus::Scheduler.new
 	include I343Auth
 	include H4Api
 
-	# re-authenticate windows live and spartan tokens
-	scheduler.every '45m' do
+	# Get fresh Spartan Tokens
+	scheduler.every('45m') do
 		if (Rails.env.development?)
 			I343Auth.update_authentication()
 		end
+	end
+
+	# Updated Services List
+	scheduler.every('1h') do
+		I343Auth.update_services_list()
 	end
