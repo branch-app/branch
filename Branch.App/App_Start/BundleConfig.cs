c49ad08837fca8e9cbece32adebed3f0db9950f1
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-11 05:38:29 +0000

    frontend, primitive background fading, and placeholder text. needs fixing up

--- Branch.App/App_Start/BundleConfig.cs
+++ Branch.App/App_Start/BundleConfig.cs
@@ -11,6 +11,17 @@ namespace Branch.App.App_Start
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 				"~/Scripts/modernizr-*"));
+
+			// le bootstrap
+			bundles.Add(new StyleBundle("~/bundles/bootstrap-custom").Include(new [] {
+				"~/Content/Styles/bootstrap-custom.min.css",
+				"~/Content/Styles/bootstrap-branch.css"
+			}));
+
+			// branch specific styles
+			bundles.Add(new StyleBundle("~/bundles/branch-specific").Include(
+				"~/Content/Styles/application.css"
+			));
 		}
 	}
 }
