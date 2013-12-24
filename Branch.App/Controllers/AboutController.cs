9ca4dc03e1574e6cd122b24f4cae84ab8ac54042
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-24 19:35:11 +0000

    About Page

---
+++ Branch.App/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Branch.App.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
