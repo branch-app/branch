7d1487f25772f392de9f96daad9dc5414a1bf55d
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-24 17:53:00 +0000

    game exception handling

--- Branch.App/Areas/Halo4/Controllers/GameController.cs
+++ Branch.App/Areas/Halo4/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using Branch.App.Areas.Halo4.Models;
 using Branch.App.Filters;
+using Branch.App.Helpers.Mvc;
 using Branch.Models.Services.Halo4._343.Responses;
 
 namespace Branch.App.Areas.Halo4.Controllers
@@ -12,8 +13,17 @@ namespace Branch.App.Areas.Halo4.Controllers
 		[ValidateH4ServiceRecordFilter]
 		public ActionResult Index(string gamertag, ServiceRecord serviceRecord, string id)
 		{
+			var game = GlobalStorage.H4WaypointManager.GetPlayerGame(serviceRecord.Gamertag, id);
+			if (game == null)
+			{
+				return FlashMessage.RedirectAndFlash(Response, RedirectToAction("Index", "ServiceRecord"),
+					FlashMessage.FlashMessageType.Failure,
+					"Unable to load Game",
+					"It seems the specified game you tried to view doesn't exist, is invalid, or has been purged by 343.");
+			}
+
 			return
-				View(new GameViewModel(serviceRecord, GlobalStorage.H4WaypointManager.GetPlayerGame(serviceRecord.Gamertag, id)));
+				View(new GameViewModel(serviceRecord, game));
 		}
 	}
 }
