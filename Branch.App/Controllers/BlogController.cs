17c9ead20bc08a4ef44721e5ae55dc83cccc8c78
Alex Reed (Xerax) <alexreed@outlook.com>
2014-04-02 15:41:50 +0100

    Blog stuff, and playlist page tweak

---
+++ Branch.App/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Branch.Models.Services.Branch;
+
+namespace Branch.App.Controllers
+{
+	public class BlogController : Controller
+	{
+		// GET: Blog
+		public ActionResult Index()
+		{
+			var blogPosts =
+				GlobalStorage.AzureStorage.Table.RetrieveMultipleEntities<BlogPostEntity>(BlogPostEntity.PartitionKeyString,
+					GlobalStorage.AzureStorage.Table.BranchCloudTable).OrderByDescending(p => p.Timestamp);
+
+			return View(blogPosts);
+		}
+	}
+}
---
+++ Branch.Models/Services/Branch/BlogPostEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Branch.Extenders;
+
+namespace Branch.Models.Services.Branch
+{
+	public sealed class BlogPostEntity : BaseEntity
+	{
+		public static readonly string RowKeyString = "Post_{0}";
+		public static readonly string PartitionKeyString = "Blog";
+
+		public static string FormatRowKey(string ending)
+		{
+			return String.Format(RowKeyString, ending.ToTitleCase());
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public BlogPostEntity(string title)
+		{
+			Id = Guid.NewGuid();
+			Title = title;
+			Slug = Title.ToSlug();
+
+			SetKeys(PartitionKeyString, FormatRowKey(Slug));
+		}
+
+		public BlogPostEntity() { }
+
+		public Guid Id { get; set; }
+
+		public string Slug { get; set; }
+
+		public string Title { get; set; }
+	}
+}
