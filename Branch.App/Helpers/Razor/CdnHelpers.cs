197112d520314a95d14ac3697e1f37b0c4368016
Alex Reed (0xdeafcafe) <alexreed@outlook.com>
2014-10-05 13:09:25 +0100

    More Reach stuff, and polish. More CDN work too

---
+++ Branch.App/Helpers/Razor/CdnHelpers.cs
@@ -0,0 +1,14 @@
+﻿namespace Branch.App.Helpers.Razor
+{
+	public static class CdnHelpers
+	{
+		public static string GetAssetLocale(bool preCompiled = true)
+		{
+#if DEBUG
+			return preCompiled ? "~/" : "/";
+#else
+			return "http://cdn.branchapp.co/cdn/";
+#endif
+		}
+	}
+}
