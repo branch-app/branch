9e70b819ed1552ba7745fafaab60a4e9d10539e8
jfransham <jef@jackfransham.com>
2015-08-11 18:19:28 +0100

    Convert to ES5, other minor changes
    * Factored out description code (will redeploy to a service)
    * Added different logs for different message types (warn, error and
    fatal redirect to stderr)

--- test/index.js
+++ test/index.js
@@ -7,27 +7,36 @@ const tc     = require('test-console');
 const test   = require('tape');
 
 test('log.info() prints correctly', function (t) {
-	let err = tc.stdout.inspectSync(() => log.info("test-error"));
-
-	let matchreg = /^info *: test-error/;
+	var err = tc.stdout.inspectSync(function () { log.info("test-error") });
+	console.log(err[0]);
+	var matchreg = /^info *: test-error/;
 	
 	t.equal(err[0].search(matchreg), 0);
 	t.end();
 });
 
 test('log.warn() prints correctly', function (t) {
-	let err = tc.stdout.inspectSync(() => log.warn("test-error"));
-
-	let matchreg = /^warn *: test-error/;
+	var err = tc.stdout.inspectSync(function () { log.warn("test-error") });
+	console.log(err[0]);
+	var matchreg = /^warn *: test-error/;
 	
 	t.equal(err[0].search(matchreg), 0);
 	t.end();
 });
 
 test('log.error() prints correctly', function (t) {
-	let err = tc.stdout.inspectSync(() => log.error("test-error"));
+	var err = tc.stdout.inspectSync(function () { log.error("test-error") });
+	console.log(err[0]);
+	var matchreg = /^error *: test-error/;
+	
+	t.equal(err[0].search(matchreg), 0);
+	t.end();
+});
 
-	let matchreg = /^error *: test-error/;
+test('log prints JSON correctly', function (t) {
+	var err = tc.stdout.inspectSync(function () { log.error("test-error", { a: "b", c: "d" }); });
+	console.log(err[0]);
+	var matchreg = /^error *: test-error\s*{((\s*\"a\"\s*:\s*\"b\"\s*|\s*\"c\"\s*:\s*"d"\s*),?){2}}/;
 	
 	t.equal(err[0].search(matchreg), 0);
 	t.end();
