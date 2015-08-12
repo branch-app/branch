16466726722a427bc624971593fd2c90a82f67d1
James Billingham <james@billingham.net>
2015-08-12 09:29:59 +0100

    Added debug/fatal checks

--- test/index.js
+++ test/index.js
@@ -1,43 +1,62 @@
 GLOBAL.projRequire = function (module) {
 	return require('../' + module);
-}
+};
 
-const log	 = projRequire('lib');
-const tc     = require('test-console');
-const test   = require('tape');
+const log = projRequire('lib');
+const tc = require('test-console');
+const test = require('tape');
+
+test('log.debug() prints correctly', function (t) {
+	var err = tc.stdout.inspectSync(function () {
+		log.debug('test-error');
+	});
+
+	t.assert(err[0].match(/^debug *: test-error/));
+	t.end();
+});
 
 test('log.info() prints correctly', function (t) {
-	var err = tc.stdout.inspectSync(function () { log.info("test-error") });
-	console.log(err[0]);
-	var matchreg = /^info *: test-error/;
-	
-	t.equal(err[0].search(matchreg), 0);
+	var err = tc.stdout.inspectSync(function () {
+		log.info('test-error');
+	});
+
+	t.assert(err[0].match(/^info *: test-error/));
 	t.end();
 });
 
 test('log.warn() prints correctly', function (t) {
-	var err = tc.stdout.inspectSync(function () { log.warn("test-error") });
-	console.log(err[0]);
-	var matchreg = /^warn *: test-error/;
-	
-	t.equal(err[0].search(matchreg), 0);
+	var err = tc.stdout.inspectSync(function () {
+		log.warn('test-error');
+	});
+
+	t.assert(err[0].match(/^warn *: test-error/));
 	t.end();
 });
 
 test('log.error() prints correctly', function (t) {
-	var err = tc.stdout.inspectSync(function () { log.error("test-error") });
-	console.log(err[0]);
-	var matchreg = /^error *: test-error/;
-	
-	t.equal(err[0].search(matchreg), 0);
+	var err = tc.stdout.inspectSync(function () {
+		log.error('test-error');
+	});
+
+	t.assert(err[0].match(/^error *: test-error/));
+	t.end();
+});
+
+test('log.fatal() prints correctly', function (t) {
+	var err = tc.stdout.inspectSync(function () {
+		log.fatal('test-error');
+	});
+
+	t.assert(err[0].match(/^fatal *: test-error/));
 	t.end();
 });
 
 test('log prints JSON correctly', function (t) {
-	var err = tc.stdout.inspectSync(function () { log.error("test-error", { a: "b", c: "d" }); });
-	console.log(err[0]);
+	var err = tc.stdout.inspectSync(function () {
+		log.error('test-error', { a: 'b', c: 'd' });
+	});
+
 	var matchreg = /^error *: test-error\s*{((\s*\"a\"\s*:\s*\"b\"\s*|\s*\"c\"\s*:\s*"d"\s*),?){2}}/;
-	
-	t.equal(err[0].search(matchreg), 0);
+	t.assert(err[0].match(matchreg));
 	t.end();
 });
