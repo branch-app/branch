0a8825a45a31fa22dac5a61d2623b2704d59ad84
Alex Forbes-Reed <github@alx.red>
2017-02-23 19:21:45 +0000

    stuff

--- contexts/service_context.go
+++ contexts/service_context.go
@@ -1,11 +1,13 @@
 package contexts
 
-import "github.com/TheTree/service-xboxlive/helpers"
+import (
+	"github.com/TheTree/helpers-go/contexts"
+	"github.com/TheTree/service-xboxlive/helpers"
+)
 
 // ServiceContext contains the service's context
 type ServiceContext struct {
-	// ServiceID is the ID of the service that owns the context.
-	ServiceID string
+	contexts.ServiceContext
 
 	// XboxLiveStore is a store holding a key-value maps for Gamertag and XUIDs.
 	XboxLiveStore *helpers.XboxLiveStore
