a70f5a809599b428fbf5a6d49aaafc017c715fdd
Alex Forbes-Reed <github@alx.red>
2017-03-05 15:27:23 +0000

    Regenerated project without active record or action mailer

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup'
+require 'rails/commands/server'
+
+module Rails
+	class Server
+		def default_options
+			super.merge({Port: 4000})
+		end
+	end
+end
