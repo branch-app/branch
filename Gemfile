dde46a5870b050187dcb769c0e1adad12d1c7afc
Alex Forbes-Reed <github@alx.red>
2017-03-05 20:59:29 +0000

    got rid of coffeescript (ew)

--- Gemfile
+++ Gemfile
@@ -14,9 +14,6 @@ gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
-
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,5 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
