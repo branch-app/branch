3df2d9dd6296b8aede889b92d31ec526e50da9a5
Alex Forbes-Reed <github@alx.red>
2017-03-06 17:22:43 +0000

    homepage images now cycle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,17 @@
 //
 //= require turbolinks
 //= require_tree .
+
+$(jQuery).ready(function () {
+	// Set copyright year
+	$('span.copyright-year').text(new Date().getUTCFullYear());
+
+	// Listen for navbar scroll changes and update navbar class
+	$(window).scroll(function () {
+		const y = $(this).scrollTop();
+		if (y < 100)
+			$('nav.navbar').removeClass('navbar-secondary');
+		else
+			$('nav.navbar').addClass('navbar-secondary');
+	});
+});
