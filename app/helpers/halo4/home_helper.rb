861dcc812979c197e867a9b3dbccdfa538e01e84
Alex Forbes-Reed <github@alx.red>
2017-03-14 10:20:46 +0000

    Fixed ‘resolve_asset_helper’ and added ‘resolve_spartan_image`

--- app/helpers/halo4/home_helper.rb
+++ app/helpers/halo4/home_helper.rb
@@ -1,12 +1,18 @@
 module Halo4::HomeHelper
 	def resolve_asset_url(asset, size)
-		asset_url = asset.asset_url.sub!('{size}', size)
-		url = Halo4Client.instance.metadata_options['settings'][asset.baseUrl]
-		local_asset = Rails.application.assets.find_asset(asset.baseUrl + asset_url)
+		asset_url = asset['assetUrl'].sub!('{size}', size.to_s)
+		base_url = asset['baseUrl']
+
+		url = Halo4Client.instance.metadata_options['settings'][base_url]
+		local_asset = Rails.application.assets.find_asset(base_url + asset_url)
 		if local_asset != nil
 			return local_asset
 		else
 			return url + asset_url
 		end
+	end
+
+	def resolve_spartan_image(gamertag, pose = 'fullbody', size = 'large')
+		return "https://spartans.svc.halowaypoint.com/players/#{gamertag}/h4/spartans/#{pose}?target=#{size}"
 	end
 end
