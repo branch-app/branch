65fe23ac66c16d1087d820fac5a7b78f46c1ee39
Alex Forbes-Reed <github@alx.red>
2017-03-14 11:46:18 +0000

    Updated configuration model

--- models/configuration.go
+++ models/configuration.go
@@ -2,13 +2,8 @@ package models
 
 // Configuration contains the structure of the service's configuration.
 type Configuration struct {
-	SentryDSN string         `json:"sentryDSN" env:"SENTRY_DSN"`
-	Port      string         `json:"port" env:"PORT"`
-	MongoDB   *MongoDBConfig `json:"mongoDb" env:"MONGO"`
-}
-
-// MongoDBConfig contains the structure of the a MongoDB configuration.
-type MongoDBConfig struct {
-	ConnectionString string `json:"connectionString" env:"CONNECTION_STRING"`
-	DatabaseName     string `json:"databaseName" env:"DATABASE_NAME"`
+	SentryDSN             string `json:"sentryDSN" env:"SENTRY_DSN"`
+	Port                  string `json:"port" env:"PORT"`
+	MongoConnectionString string `json:"mongoConnectionString" env:"MONGO_CONNECTION_STRING"`
+	MongoDatabaseName     string `json:"mongoDatabaseName" env:"MONGO_DATABASE_NAME"`
 }
