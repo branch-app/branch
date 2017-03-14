ebb3f519dfec22e3b0989a470adc3586581cfa42
Alex Forbes-Reed <github@alx.red>
2017-03-14 11:32:22 +0000

    Added env tags to configuration

--- models/configuration.go
+++ models/configuration.go
@@ -2,12 +2,13 @@ package models
 
 // Configuration contains the structure of the service's configuration.
 type Configuration struct {
-	SentryDSN string         `json:"sentryDSN"`
-	Port      string         `json:"port"`
-	MongoDB   *MongoDBConfig `json:"mongoDb"`
+	SentryDSN string         `json:"sentryDSN" env:"SENTRY_DSN"`
+	Port      string         `json:"port" env:"PORT"`
+	MongoDB   *MongoDBConfig `json:"mongoDb" env:"MONGO"`
 }
 
+// MongoDBConfig contains the structure of the a MongoDB configuration.
 type MongoDBConfig struct {
-	ConnectionString string `json:"connectionString"`
-	DatabaseName     string `json:"databaseName"`
+	ConnectionString string `json:"connectionString" env:"CONNECTION_STRING"`
+	DatabaseName     string `json:"databaseName" env:"DATABASE_NAME"`
 }
