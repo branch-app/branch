b997771880d544ae387d8ce2af6d8e07fed97151
Alex Forbes-Reed <github@alx.red>
2017-03-16 11:53:24 +0000

    various halo 4 service record helpers
    - Rank Progression Percentage
    - Rank Progression Comparison
    - Played Games Spread (a/ legend) to JSON

--- app/helpers/halo4/service_record_helper.rb
+++ app/helpers/halo4/service_record_helper.rb
@@ -1,3 +1,38 @@
 module Halo4::ServiceRecordHelper
-	
+	def rank_progression_percentage
+		xp = @service_record['xp']
+		rank_start_xp = @service_record['rankStartXp']
+		next_rank_start_xp = @service_record['nextRankStartXp']
+
+		percentage = generate_percentage(xp - rank_start_xp, next_rank_start_xp - rank_start_xp)
+		percentage = 100 if @service_record['nextRankName'] == ''
+
+		return percentage
+	end
+
+	def rank_progression_comparison
+		xp = @service_record['xp']
+		rank_start_xp = @service_record['rankStartXp']
+		next_rank_start_xp = @service_record['nextRankStartXp']
+
+		return ' - Max Rank Achieved' if @service_record['nextRankName'] == ''
+
+		return " (#{xp}/#{next_rank_start_xp})"
+	end
+
+	def played_games_spread_json
+		played_games = Array.new(@service_record['gameModes'].length)
+		@service_record['gameModes'].each_with_index do |mode, index|
+			played_games[index] = mode['totalGamesStarted']
+		end
+		return played_games.to_json
+	end
+
+	def played_games_spread_legend_json
+		played_games_legend = Array.new(@service_record['gameModes'].length)
+		@service_record['gameModes'].each_with_index do |mode, index|
+			played_games_legend[index] = mode['name']
+		end
+		return played_games_legend.to_json
+	end
 end
