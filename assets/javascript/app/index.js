1215a91404d745d576be1f16340cc20dc8b3a27f
Alex Forbes-Reed <github@alx.red>
2017-05-12 21:53:05 +0100

    loads of eslint work, client side js coming all danky

--- assets/javascript/app/index.js
+++ assets/javascript/app/index.js
@@ -1,21 +1,51 @@
 import $ from 'jquery';
 
-export default class App {
-	options;
+type Options = {
 
-	constructor(options) {
+};
+
+export default class App {
+	options: Options;
+	above: ?boolean = void 0;
+	navbarStateChangeIndex: number = 55;
+
+	constructor(options: Options) {
 		this.options = options;
 
+		this.setupView();
 		this.setupEvents();
-		this.setup();
+		this.updateNavStyle();
 	}
 
-	setup() {
+	setupView() {
 		// Set copyright year
 		$('span.copyright-year').text(new Date().getUTCFullYear());
+
+		// Enable popover
+		$('[data-toggle=popover]').popover({
+			html: true,
+			content: () => {
+				const content = $(this).attr('data-popover-content');
+
+				return $(content)
+					.children('.popover-body')
+					.html();
+			},
+		});
+
+		// Enable tool-tips
+		$('[data-toggle="tooltip"]').tooltip();
 	}
 
 	setupEvents() {
+		$(window).scroll(this.updateNavStyle);
+	}
 
+	updateNavStyle() {
+		const prevValue = this.above;
+
+		this.above = $(window).scrollTop() < this.navbarStateChangeIndex;
+		if (prevValue === this.above) return;
+		$('nav.navbar')[`${this.above ? 'remove' : 'add'}Class`]('navbar-secondary');
 	}
 }
