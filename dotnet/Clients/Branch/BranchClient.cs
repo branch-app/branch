42b62f8f920ce0706282b5685a74d9d937523587
Alex Forbes-Reed <github@alx.red>
2018-08-20 09:59:38 +0100

    Added client for interacting with branch services

---
+++ dotnet/Clients/Branch/BranchClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Branch.Clients.Json;
+using Branch.Clients.Json.Models;
+using Branch.Packages.Exceptions;
+
+namespace Branch.Clients.Branch
+{
+	public class BranchClient
+	{
+		internal JsonClient Client { get; set; }
+
+		public BranchClient(string baseUrl, string key, Options options = null)
+		{
+			options = options ?? new Options();
+			options.Headers.Add("Authorization", $"Internal 01.{key}");
+
+			Client = new JsonClient(baseUrl, options);
+		}
+
+		public async Task<TRes> Do<TReq, TRes>(string path, TReq body, Options options = null)
+			where TReq : class
+			where TRes : class
+		{
+			return await Client.Do<TReq, TRes, BranchException>("POST", path, null, body, options);
+		}
+	}
+}
