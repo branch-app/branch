f32135c834b51dc257caa9ae63e41d66569f053d
Alex Forbes-Reed <github@alx.red>
2018-09-22 14:56:26 +0100

    New branch exception

--- dotnet/Packages/Exceptions/BranchException.cs
+++ dotnet/Packages/Exceptions/BranchException.cs
@@ -1,10 +1,43 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Branch.Packages.Exceptions
 {
-	public class BranchException : Exception
+	public class BranchException : AggregateException
 	{
-		public BranchException(string message)
-			: base(message) { }
+		public BranchException() { }
+
+		public BranchException(string message) : base(message) { }
+
+		public BranchException(string message, Dictionary<string, object> data)
+			: base(message)
+		{
+			if (data == null)
+				return;
+
+			foreach (var pair in data)
+				Data.Add(pair.Key, pair.Value);
+		}
+
+		public BranchException(string message, Dictionary<string, object> data, Exception ex)
+			: base(message, ex)
+		{
+			if (data == null)
+				return;
+
+			foreach (var pair in data)
+				Data.Add(pair.Key, pair.Value);
+		}
+
+		public BranchException(string message, Dictionary<string, object> data, IEnumerable<Exception> exs)
+			: base(message, exs)
+		{
+			if (data == null)
+				return;
+
+			foreach (var pair in data)
+				Data.Add(pair.Key, pair.Value);
+		}
 	}
 }
