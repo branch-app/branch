6141c3583d102f5299cb2dcbb24fc9f283e42032
Alex Forbes-Reed <github@alx.red>
2018-09-22 14:55:26 +0100

    Added identity client

---
+++ dotnet/Clients/Identity/identityClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Branch.Clients.Branch;
+using System.Threading;
+using System.Threading.Tasks;
+using Branch.Clients.Json.Models;
+using Branch.Packages.Contracts.ServiceIdentity;
+
+namespace Branch.Clients.Identity
+{
+	public class IdentityClient : BranchClient, IService
+	{
+		public IdentityClient(string baseUrl, string key)
+			: base(baseUrl, key)
+		{ }
+
+		public async Task<ResGetXboxLiveIdentity> GetXboxLiveIdentity(ReqGetXboxLiveIdentity req)
+		{
+			return await base.Do<ReqGetXboxLiveIdentity, ResGetXboxLiveIdentity>("1/2018-08-19/get_xboxlive_identity", req);
+		}
+	}
+}
