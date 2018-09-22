4ace98044d8e6e469065f829c54183e1d31f38d1
Alex Forbes-Reed <github@alx.red>
2018-09-22 14:56:33 +0100

    s3 creds mane

---
+++ dotnet/Packages/Models/Common/Config/S3Credentials.cs
@@ -0,0 +1,11 @@
+namespace Branch.Packages.Models.Common.Config
+{
+	public class S3Credentials
+	{
+		public string AccessKeyId { get; set; }
+
+		public string SecretAccessKey { get; set; }
+
+		public string Region { get; set; }
+	}
+}
