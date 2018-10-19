c48e092e9f07f3ac6f8e1c620244aae3c070a036
Alex Forbes-Reed <github@alx.red>
2018-10-19 18:17:34 +0100

    Added other fidileity type

--- dotnet/Packages/Enums/Halo4/DateFidelity.cs
+++ dotnet/Packages/Enums/Halo4/DateFidelity.cs
@@ -8,7 +8,10 @@ namespace Branch.Packages.Enums.Halo4
 	[JsonConverter(typeof(StringEnumConverter))]
 	public enum DateFidelity
 	{
-		[EnumMember(Value = "by-day")]
-		ByDay = 1,
+		[EnumMember(Value = "rough")]
+		Rough = 1,
+
+		[EnumMember(Value = "exact")]
+		Exact = 2,
 	}
 }
