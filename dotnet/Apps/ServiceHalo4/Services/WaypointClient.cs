5ec4c0bab0e7b7c1aa898dbda56c5e4b976fc350
Alex Forbes-Reed <github@alx.red>
2018-12-31 14:58:05 +0000

    Abstract out caching logic from WaypointClient

--- dotnet/Apps/ServiceHalo4/Services/WaypointClient.cs
+++ dotnet/Apps/ServiceHalo4/Services/WaypointClient.cs
@@ -8,6 +8,7 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using Branch.Apps.ServiceHalo4.Enums.Waypoint;
+using Branch.Clients.Cache;
 using Branch.Clients.Token;
 using Branch.Clients.Identity;
 using Branch.Clients.Json;
@@ -28,10 +29,9 @@ using External = Branch.Apps.ServiceHalo4.Models.Waypoint;
 
 namespace Branch.Apps.ServiceHalo4.Services
 {
-	public partial class WaypointClient
+	public partial class WaypointClient : CacheClient
 	{
 		private TokenClient tokenClient { get; }
-		private AmazonS3Client s3Client { get; }
 		private JsonClient presenceClient { get; }
 		private JsonClient statsClient { get; }
 		private JsonClient settingsClient { get; }
@@ -47,31 +47,15 @@ namespace Branch.Apps.ServiceHalo4.Services
 		private const string storageBucket = "branch-app-stats";
 
 		public WaypointClient(TokenClient tokenClient, IdentityClient identityClient, AmazonS3Client s3Client)
+			: base(storageBucket, s3Client)
 		{
 			this.tokenClient = tokenClient;
-			this.s3Client = s3Client;
 			this.transpiler = new Transpiler();
 			this.enricher = new Enricher(identityClient);
 			this.presenceClient = new JsonClient(presenceUrl);
 			this.statsClient = new JsonClient(statsUrl);
 			this.settingsClient = new JsonClient(settingsUrl);
 			this.optionsClient = new JsonClient(optionsUrl);
-		}
-
-		private async Task<ICacheInfo> fetchContentCacheInfo(string bucketKey)
-		{
-			try
-			{
-				var resp = await s3Client.GetObjectMetadataAsync(storageBucket, bucketKey);
-				var contentCreation = DateTime.Parse(resp.Metadata["x-amz-meta-content-creation"]);
-				var contentExpiration = DateTime.Parse(resp.Metadata["x-amz-meta-content-expiration"]);
-
-				return new CacheInfo(contentCreation, contentExpiration);
-			}
-			catch (AmazonS3Exception ex) when (ex.ErrorCode == "NotFound")
-			{
-				return null;
-			}
 		}
 
 		private async Task<T> requestWaypointData<T>(string path, Dictionary<string, string> query, string bucketKey)
@@ -105,73 +89,6 @@ namespace Branch.Apps.ServiceHalo4.Services
 			}
 		}
 
-		private async Task<T> fetchContent<T>(string bucketKey)
-		{
-			try
-			{
-				var response = await s3Client.GetObjectAsync(storageBucket, bucketKey);
-
-				using (var ms = new MemoryStream())
-				using (var sr = new StreamReader(ms))
-				using (var jr = new JsonTextReader(sr))
-				{
-					await response.ResponseStream.CopyToAsync(ms);
-					ms.Seek(0, SeekOrigin.Begin);
-
-					var serializer = new JsonSerializer
-					{
-						ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() }
-					};
-
-					return serializer.Deserialize<T>(jr);
-				}
-			}
-			catch (AmazonS3Exception ex)
-			{
-				if (ex.ErrorCode != "NotFound")
-					throw;
-
-				throw
-					new BranchException(
-						"cache_not_found",
-						new Dictionary<string, object> { { "key", bucketKey } }
-					);
-			}
-		}
-
-		private async Task cacheContent<T>(string key, T content, ICacheInfo cacheInfo)
-		{
-			using (var ms = new MemoryStream())
-			using (var sw = new StreamWriter(ms))
-			using (var jw = new JsonTextWriter(sw))
-			{
-				var serializer = new JsonSerializer();
-				var now = DateTime.UtcNow;
-
-				serializer.ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() };
-				serializer.Serialize(jw, content);
-				await sw.FlushAsync();
-
-				var putReq = new PutObjectRequest
-				{
-					BucketName = storageBucket,
-					Key = key,
-					ContentType = "application/json",
-
-					// We use the StreamWriter BaseStream, as apparently using a
-					// MemoryStream breaks this part and only uploads an initial partial
-					// segment of the data? Fuck knows
-					InputStream = sw.BaseStream,
-				};
-
-				putReq.Metadata["content-expiration"] = cacheInfo.ExpiresAt.ToISOString();
-				putReq.Metadata["content-creation"] = cacheInfo.CachedAt.ToISOString();
-				putReq.Metadata["content-hash"] = Sha256.HashContent(ms).ToHexString();
-
-				await s3Client.PutObjectAsync(putReq);
-			}
-		}
-
 		private async Task<Dictionary<string, string>> getTokenHeaders()
 		{
 			var resp = await tokenClient.GetHalo4Token(new ReqGetHalo4Token());
@@ -184,7 +101,7 @@ namespace Branch.Apps.ServiceHalo4.Services
 			var path = $"players/{identity.Gamertag}/h4/servicerecord";
 			var key = $"halo-4/service-record/{identity.XUIDStr}.json";
 			var expire = TimeSpan.FromMinutes(10);
-			var cacheInfo = await fetchContentCacheInfo(key);
+			var cacheInfo = await fetchCacheInfo(key);
 
 			if (cacheInfo != null && cacheInfo.IsFresh())
 				return (await fetchContent<ServiceRecordResponse>(key), cacheInfo);
@@ -195,10 +112,9 @@ namespace Branch.Apps.ServiceHalo4.Services
 				return (await fetchContent<ServiceRecordResponse>(key), cacheInfo);
 
 			// Transpile and enrich content
+			var finalCacheInfo = new CacheInfo(DateTime.UtcNow, expire);
 			var final = transpiler.ServiceRecord(response);
 			final = await enricher.ServiceRecord(final, response);
-
-			var finalCacheInfo = new CacheInfo(DateTime.UtcNow, expire);
 
 			TaskExt.FireAndForget(() => cacheContent(key, final, finalCacheInfo));
 
@@ -218,7 +134,7 @@ namespace Branch.Apps.ServiceHalo4.Services
 			var path = $"players/{identity.Gamertag}/h4/matches";
 			var key = $"halo-4/recent-matches/{identity.XUIDStr}-{gameModeStr}-{startAt}-{newCount}.json";
 			var expire = TimeSpan.FromMinutes(10);
-			var cacheInfo = await fetchContentCacheInfo(key);
+			var cacheInfo = await fetchCacheInfo(key);
 
 			if (cacheInfo != null && cacheInfo.IsFresh())
 				return (await fetchContent<RecentMatchesResponse>(key), cacheInfo);
