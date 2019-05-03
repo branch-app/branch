3d89d02d3206b726e6bb8c9ad2d5eab3d3480a04
Alex Forbes-Reed <github@alx.red>
2019-05-03 03:06:51 +0100

    and finallllly

--- dotnet/Apps/ServiceHalo2/Services/BnetClient.cs
+++ dotnet/Apps/ServiceHalo2/Services/BnetClient.cs
@@ -95,78 +95,85 @@ namespace Branch.Apps.ServiceHalo2.Services
 			using (_logger.BeginScope($"Caching Halo 2 Service Record for {gamertag}"))
 			using (var page = await _browser.NewPageAsync())
 			{
-				var url = string.Format(_serviceRecordUrl, gamertag);
-				await page.GoToAsync(url);
-
-				_logger.LogInformation($"Checking if {gamertag} has played Halo 2");
-
-				var playerFound = await page.EvaluateFunctionAsync<bool>(@"
-					() => !window.document.querySelector('.rgNoRecords')
-				");
-
-				if (!playerFound)
+				try
 				{
-					// TODO(0xdeafcafe): Record player never played
-					_logger.LogInformation($"{gamertag} never played");
-					await _dbClient.SetCacheMeta(cacheMetaIdent, "failed", new BaeException("player_not_found"));
+					var url = string.Format(_serviceRecordUrl, gamertag);
+					await page.GoToAsync(url);
+
+					_logger.LogInformation($"Checking if {gamertag} has played Halo 2");
+
+					var playerFound = await page.EvaluateFunctionAsync<bool>(@"
+						() => !window.document.querySelector('.rgNoRecords')
+					");
+
+					if (!playerFound)
+					{
+						// TODO(0xdeafcafe): Record player never played
+						_logger.LogInformation($"{gamertag} never played");
+						await _dbClient.SetCacheMeta(cacheMetaIdent, "failed", new BaeException("player_not_found"));
+
+						return true;
+					}
+
+					var pageInfo = await page.EvaluateFunctionAsync<Dictionary<string, string>>(@"
+						() => {
+							const sr = window.document.querySelector('#ctl00_mainContent_bnetpgl_identityStrip_div1');
+							const gamertag = sr.querySelector('.header_stats > ul > li > h3').innerText;
+							const emblemUrl = sr.querySelector('.profile_picA > img').src;
+							const clanInfo = sr.querySelector('.firstline > a').innerText;
+							const playerInfo = sr.querySelector('.secondline').innerText;
+
+							return {
+								gamertag,
+								emblemUrl,
+								clanInfo,
+								playerInfo,
+							};
+						}
+					");
+
+					var sr = Regex.Matches(pageInfo["playerInfo"], _serviceRecordHeaderRegex, RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase);
+					if (sr.Count != 5)
+					{
+						var ex = new FormatException("service record regex failed");
+						ex.Data.Add("PlayerInfo", pageInfo["playerInfo"]);
+
+						_logger.LogError(ex, "Unable to parse Service Record");
+						_sentry.CaptureException(ex);
+
+						return false;
+					}
+
+					var matchPageCount = await page.EvaluateFunctionAsync<int>(@"
+						() => window.document.querySelector('.rgArrPart2 > a:nth-child(2)').href.split('=').slice(-1).pop()
+					");
+					_logger.LogInformation($"Recent match page count for {gamertag} is {matchPageCount}");
+					await queueRecentMatchPages(gamertag, matchPageCount);
+
+					var serviceRecord = new ServiceRecord
+					{
+						Gamertag = pageInfo["gamertag"],
+						EmblemUrl = pageInfo["emblemUrl"],
+						ClanName = pageInfo["clanInfo"],
+						TotalGames = int.Parse(sr[0].Groups[1].Value),
+						LastPlayed = DateTime.Parse(sr[1].Groups[2].Value),
+						TotalKills = int.Parse(sr[2].Groups[3].Value),
+						TotalDeaths = int.Parse(sr[3].Groups[4].Value),
+						TotalAssists = int.Parse(sr[4].Groups[5].Value),
+					};
+
+					await Task.WhenAll(new Task[]
+					{
+						_dbClient.SetCacheMeta(cacheMetaIdent, "complete"),
+						_dbClient.SetServiceRecord(serviceRecord),
+					});
 
 					return true;
 				}
-
-				var pageInfo = await page.EvaluateFunctionAsync<Dictionary<string, string>>(@"
-					() => {
-						const sr = window.document.querySelector('#ctl00_mainContent_bnetpgl_identityStrip_div1');
-						const gamertag = sr.querySelector('.header_stats > ul > li > h3').innerText;
-						const emblemUrl = sr.querySelector('.profile_picA > img').src;
-						const clanInfo = sr.querySelector('.firstline > a').innerText;
-						const playerInfo = sr.querySelector('.secondline').innerText;
-
-						return {
-							gamertag,
-							emblemUrl,
-							clanInfo,
-							playerInfo,
-						};
-					}
-				");
-
-				var sr = Regex.Matches(pageInfo["playerInfo"], _serviceRecordHeaderRegex, RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase);
-				if (sr.Count != 5)
+				finally
 				{
-					var ex = new FormatException("service record regex failed");
-					ex.Data.Add("PlayerInfo", pageInfo["playerInfo"]);
-
-					_logger.LogError(ex, "Unable to parse Service Record");
-					_sentry.CaptureException(ex);
-
-					return false;
+					await page.CloseAsync();
 				}
-
-				var matchPageCount = await page.EvaluateFunctionAsync<int>(@"
-					() => window.document.querySelector('.rgArrPart2 > a:nth-child(2)').href.split('=').slice(-1).pop()
-				");
-				_logger.LogInformation($"Recent match page count for {gamertag} is {matchPageCount}");
-				await queueRecentMatchPages(gamertag, matchPageCount);
-
-				var serviceRecord = new ServiceRecord
-				{
-					Gamertag = pageInfo["gamertag"],
-					EmblemUrl = pageInfo["emblemUrl"],
-					ClanName = pageInfo["clanInfo"],
-					TotalGames = int.Parse(sr[0].Groups[1].Value),
-					LastPlayed = DateTime.Parse(sr[1].Groups[2].Value),
-					TotalKills = int.Parse(sr[2].Groups[3].Value),
-					TotalDeaths = int.Parse(sr[3].Groups[4].Value),
-					TotalAssists = int.Parse(sr[4].Groups[5].Value),
-				};
-
-				await Task.WhenAll(new Task[]
-				{
-					_dbClient.SetCacheMeta(cacheMetaIdent, "complete"),
-					_dbClient.SetServiceRecord(serviceRecord),
-				});
-
-				return true;
 			}
 		}
 
