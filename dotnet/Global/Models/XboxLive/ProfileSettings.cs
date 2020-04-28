60eb4c18089d62a99eec7b308251c86c54741fea
Alexander Forbes-Reed <git@alx.red>
2020-04-28 18:19:11 +0100

    and models...

---
+++ dotnet/Global/Models/XboxLive/ProfileSettings.cs
@@ -0,0 +1,7 @@
+namespace Branch.Global.Models.XboxLive
+{
+	public class ProfileSettings
+	{
+		public ProfileUser[] ProfileUsers { get; set; }
+	}
+}
---
+++ dotnet/Global/Models/XboxLive/ProfileUserSetting.cs
@@ -0,0 +1,9 @@
+namespace Branch.Global.Models.XboxLive
+{
+	public class ProfileUserSetting
+	{
+		public string ID { get; set; }
+
+		public string Value { get; set; }
+	}
+}
